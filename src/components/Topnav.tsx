c46b83142eab5e0e9f5f41c2358ae2713329fda7
hikmet <aydin96tr@gmail.com>
2023-02-11 18:19:59 +0300

    e

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import { Appbar, Searchbar } from "react-native-paper";
+import { Appbar } from "react-native-paper";
+import { Search } from "./Search";
 
 export function Topnav({}) {
-  const [searchQuery, setSearchQuery] = useState("");
-  const onChangeSearch = (query: string) => setSearchQuery(query);
-
   return (
     <Appbar.Header className="bg-white">
       <Appbar.Action icon="tune-variant" onPress={() => console.log("test")} />
-      <Searchbar
-        className="w-5/6 rounded-full bg-gray-50 shadow-none h-[40]"
-        placeholder="Şarkı yada sanatçı arayın"
-        placeholderTextColor="#aaa"
-        inputStyle={{ fontSize: 16 }}
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-      />
+      <Search />
     </Appbar.Header>
   );
 }
