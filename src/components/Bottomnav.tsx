12ebb8406be5b3cf514e28479294e3d7090e7749
hikmet <aydin96tr@gmail.com>
2023-02-11 17:00:17 +0300

    e

--- src/components/Bottomnav.tsx
+++ src/components/Bottomnav.tsx
@@ -1,15 +1,15 @@
-import { Appbar, Text } from "react-native-paper";
 import { View } from "react-native";
+import { Appbar, Text } from "react-native-paper";
 
 export function Bottomnav({}) {
   return (
-    <Appbar.Header className="bg-white pb-4 justify-around border-t-2 border-gray-100">
-      <Appbar.Action icon="album" size={64} className="pb-2" />
+    <Appbar.Header className="bg-white justify-around border-t-2 border-gray-100">
+      <Appbar.Action icon="album" size={64} className="pb-8" />
       <View className="pb-2">
         <Text className="text-lg font-bold">Şarkı adı</Text>
         <Text className="text-sm text-gray-400">Sanatçı</Text>
       </View>
-      <View className="flex-row">
+      <View className="flex-row pb-2">
         <Appbar.Action icon="play-circle-outline" size={32} />
         <Appbar.Action icon="skip-next-outline" size={32} />
       </View>
