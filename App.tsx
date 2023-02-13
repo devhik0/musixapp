2d407663deb7ae141b44a162310c8e4bfb08e704
hikmet <aydin96tr@gmail.com>
2023-02-13 21:35:45 +0300

    navigation setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { StatusBar, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
 import { Text } from "react-native-paper";
-import { Bottomnav } from "src/components/Bottomnav";
-import { Linknav } from "src/components/Linknav";
-import { Menu } from "src/components/Menu";
-import { Songs } from "src/components/Songs";
-import { Topnav } from "src/components/Topnav";
+
+// Şarkılar
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+function OtherScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Other!</Text>
+    </View>
+  );
+}
+function NotherScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Nother!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View className="bg-white flex-1">
-      <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-      <Topnav />
-      <Menu />
-      <Text className="font-extrabold text-2xl m-2">Tüm Şarkılar</Text>
-      <Linknav />
-      <Songs />
-      <Bottomnav />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Other" component={OtherScreen} />
+        <Tab.Screen name="Nother" component={NotherScreen} />
+      </Tab.Navigator>
+      {/* <View className="bg-white flex-1">
+        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+        <Topnav />
+        <Menu />
+        <Text className="font-extrabold text-2xl m-2">Tüm Şarkılar</Text>
+        <Linknav />
+        <Songs />
+        <Bottomnav />
+      </View> */}
+    </NavigationContainer>
   );
 }
