d2850144a364b1dcf19d093aa8c58a1612d827ef
hikmet <aydin96tr@gmail.com>
2023-03-20 20:25:49 +0300

    fix

--- src/components/menu/fav/FavMenuItems.tsx
+++ src/components/menu/fav/FavMenuItems.tsx
@@ -1,15 +1,26 @@
-import { FlatList, Text, View } from "react-native";
-import { FavMenuItem } from "./FavMenuItem";
+import { useState } from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { RadioButton } from "react-native-paper";
 
 export const FavMenuItems = () => {
   const sortby = ["Son eklenene", "Ada", "Artiste"];
+  const [checked, setChecked] = useState(sortby[0]);
 
   return (
     <View>
       <Text className="text-center text-lg font-bold mb-2">Şarkıları sırala</Text>
       <FlatList
         data={sortby}
-        renderItem={({ item, index }) => <FavMenuItem item={item} index={index} sortby={sortby} />}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            className="flex-row items-center justify-between px-2"
+            style={{ backgroundColor: checked === item ? "#EACFFA" : "transparent" }}
+            onPress={() => setChecked(item)}
+          >
+            <Text className="text-[16px] mr-4">{sortby[index]} göre</Text>
+            <RadioButton value={item} status={checked === item ? "checked" : "unchecked"} />
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
--- src/screens/songs/SongsScreenHeaderMenuItems.tsx
+++ src/screens/songs/SongsScreenHeaderMenuItems.tsx
@@ -1,13 +1,24 @@
-import { FlatList } from "react-native";
-import { SongsScreenHeaderMenuItem } from "./SongsScreenHeaderMenuItem";
+import { useState } from "react";
+import { FlatList, Text, TouchableOpacity } from "react-native";
+import { RadioButton } from "react-native-paper";
 
 export const SongsScreenHeaderMenuItems = () => {
   const sortby = ["Son eklenene", "Ada", "Artiste"];
+  const [checked, setChecked] = useState(sortby[0]);
 
   return (
     <FlatList
       data={sortby}
-      renderItem={({ item, index }) => <SongsScreenHeaderMenuItem item={item} index={index} sortby={sortby} />}
+      renderItem={({ item, index }) => (
+        <TouchableOpacity
+          className="flex-row items-center justify-between px-2"
+          style={{ backgroundColor: checked === item ? "#EACFFA" : "transparent" }}
+          onPress={() => setChecked(item)}
+        >
+          <Text className="text-[16px] mr-4">{sortby[index]} göre</Text>
+          <RadioButton value={item} status={checked === item ? "checked" : "unchecked"} />
+        </TouchableOpacity>
+      )}
     />
   );
 };
